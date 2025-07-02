da395ba75c1b347d86ac01abe500583724de9f20
Your Name <your-email@example.com>
2025-07-02 17:21:54 +0530

    Firebase Integration

---
+++ Firebase/client.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import {getAuth} from "firebase-admin/auth";
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAV-49ZNXm26_uOezqUUf6jNew1xTOuTJs",
+    authDomain: "nexprep-c1462.firebaseapp.com",
+    projectId: "nexprep-c1462",
+    storageBucket: "nexprep-c1462.firebasestorage.app",
+    messagingSenderId: "1093054324958",
+    appId: "1:1093054324958:web:33801acce857a652b5624f",
+    measurementId: "G-9QQGQDC10Q"
+};
+
+// Initialize Firebase
+const app =!getApps.length ? initializeApp(firebaseConfig): getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
